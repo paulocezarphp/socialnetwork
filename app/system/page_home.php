9bfdb5366bcc4387fba76c8ab20e75f16315ca4a
Cezar Leigos <cezarleigos28@outlook.com>
2021-01-30 18:00:03 -0300

    v0.03 - Defining a little more about the basic structure of the project
    This time we started to improve the appearance of the homepage of our social network, something very simple so that we can start the project with a good base of how the objects will be positioned for the user.

--- app/system/page_home.php
+++ app/system/page_home.php
@@ -11,6 +11,8 @@
 	<div class="grid">
 		<div class="grid-left">
 			<h3>Pages</h3>
+
+
 		</div>
 		<div class="grid-center">
 			<div class="cardpost">
@@ -84,10 +86,126 @@
 						Mauris hendrerit varius inceptos, etiam mauris purus id viverra orci enim, felis euismod blandit litora euismod massa lectus. vehicula sociosqu placerat morbi eget dui augue aliquam pharetra imperdiet. 
 					</div>
 				</div>
-			</div>	
+			</div>
+            
+            
+            <button class="grid-load-more-button"> Load More </button>
+
+
 		</div>
 		<div class="grid-right">
 			<h3>Friends</h3>
+            <br>
+
+            <div class="box">
+
+            	<div class="friend">
+            	   <img src="app/system/image/friend1.jpg" class="friend-image">
+            	   <div>
+            	   	  <div class="friend-name">Friends A</div>
+            	   </div>
+                </div>
+                
+                <div class="friend">
+            	   <img src="app/system/image/friend2.jpg" class="friend-image">
+            	   <div>
+            	   	  <div class="friend-name">Friends B</div>
+            	   </div>
+                </div>
+
+                <div class="friend">
+            	   <img src="app/system/image/friend3.jpg" class="friend-image">
+            	   <div>
+            	   	  <div class="friend-name">Friends C</div>
+            	   </div>
+                </div>
+
+                <div class="friend">
+            	   <img src="app/system/image/friend1.jpg" class="friend-image">
+            	   <div>
+            	   	  <div class="friend-name">Friends A</div>
+            	   </div>
+                </div>
+                
+                <div class="friend">
+            	   <img src="app/system/image/friend2.jpg" class="friend-image">
+            	   <div>
+            	   	  <div class="friend-name">Friends B</div>
+            	   </div>
+                </div>
+
+                <div class="friend">
+            	   <img src="app/system/image/friend3.jpg" class="friend-image">
+            	   <div>
+            	   	  <div class="friend-name">Friends C</div>
+            	   </div>
+                </div>
+
+                <div class="friend">
+            	   <img src="app/system/image/friend1.jpg" class="friend-image">
+            	   <div>
+            	   	  <div class="friend-name">Friends A</div>
+            	   </div>
+                </div>
+                
+                <div class="friend">
+            	   <img src="app/system/image/friend2.jpg" class="friend-image">
+            	   <div>
+            	   	  <div class="friend-name">Friends B</div>
+            	   </div>
+                </div>
+
+                <div class="friend">
+            	   <img src="app/system/image/friend3.jpg" class="friend-image">
+            	   <div>
+            	   	  <div class="friend-name">Friends C</div>
+            	   </div>
+                </div>
+
+                <div class="friend">
+            	   <img src="app/system/image/friend1.jpg" class="friend-image">
+            	   <div>
+            	   	  <div class="friend-name">Friends A</div>
+            	   </div>
+                </div>
+                
+                <div class="friend">
+            	   <img src="app/system/image/friend2.jpg" class="friend-image">
+            	   <div>
+            	   	  <div class="friend-name">Friends B</div>
+            	   </div>
+                </div>
+
+                <div class="friend">
+            	   <img src="app/system/image/friend3.jpg" class="friend-image">
+            	   <div>
+            	   	  <div class="friend-name">Friends C</div>
+            	   </div>
+                </div>
+
+                <div class="friend">
+            	   <img src="app/system/image/friend1.jpg" class="friend-image">
+            	   <div>
+            	   	  <div class="friend-name">Friends A</div>
+            	   </div>
+                </div>
+                
+                <div class="friend">
+            	   <img src="app/system/image/friend2.jpg" class="friend-image">
+            	   <div>
+            	   	  <div class="friend-name">Friends B</div>
+            	   </div>
+                </div>
+
+                <div class="friend">
+            	   <img src="app/system/image/friend3.jpg" class="friend-image">
+            	   <div>
+            	   	  <div class="friend-name">Friends C</div>
+            	   </div>
+                </div>
+
+            </div>
+
 		</div>
 	</div>	
 </div>	
